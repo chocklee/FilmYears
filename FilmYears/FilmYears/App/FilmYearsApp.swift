8bb065844df1a7c7fe2755dd9af4847a1595eacb
lichanghao <chocklee@yeah.net>
2026-07-29 21:14:21 +0800

    Revert "fix: 返回按钮颜色改为 textSecondary（UIKit appearance）"
    This reverts commit 592e417f591581ff7ebbeef035b8947b929164f0.

--- FilmYears/FilmYears/App/FilmYearsApp.swift
+++ FilmYears/FilmYears/App/FilmYearsApp.swift
@@ -1,21 +1,15 @@
 import SwiftUI
 import SwiftData
-import UIKit
 
 @main
 struct FilmYearsApp: App {
     let persistenceController = PersistenceController.shared
 
-    init() {
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithDefaultBackground()
-        UINavigationBar.appearance().tintColor = UIColor(Color.textSecondary)
-    }
-
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .preferredColorScheme(.dark)
+                .tint(.textSecondary)
         }
         .modelContainer(persistenceController.container)
     }
