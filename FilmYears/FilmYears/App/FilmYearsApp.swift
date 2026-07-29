59a648cb3205629121ff69e1cebc0edd541c69cb
lichanghao <chocklee@yeah.net>
2026-07-29 21:11:58 +0800

    fix: 返回按钮颜色 .textSecondary（NavigationStack tint）

--- FilmYears/FilmYears/App/FilmYearsApp.swift
+++ FilmYears/FilmYears/App/FilmYearsApp.swift
@@ -1,16 +1,9 @@
 import SwiftUI
 import SwiftData
-import UIKit
 
 @main
 struct FilmYearsApp: App {
     let persistenceController = PersistenceController.shared
-
-    init() {
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithDefaultBackground()
-        UINavigationBar.appearance().tintColor = UIColor(Color.textSecondary)
-    }
 
     var body: some Scene {
         WindowGroup {
